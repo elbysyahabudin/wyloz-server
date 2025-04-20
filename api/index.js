c69a7bfd8034131693805fa9d86f956352380a2f
elbysyahabudin <elbysyahabudinn@gmail.com>
2025-04-20 19:02:02 +0700

    first keuda

--- api/index.js
+++ api/index.js
@@ -323,8 +323,8 @@ app.get('/auth/google',
     }
   });
 
-// app.listen(5000, () => {
-//     console.log("Server listening on port 3000");
-// });
+app.listen(5000, () => {
+    console.log("Server listening on port 3000");
+});
 
-module.exports = app;
+// module.exports = app; 
